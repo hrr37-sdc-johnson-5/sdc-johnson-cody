bc7b2456c5a41a84c12662ca63671d71f8e789c9
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-03-29 19:54:51 -0400

    begin to convert hardcode comment section to react component based

---
+++ client/components/CommentList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Comment from './Comment.jsx';
+
+class CommentList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render () {
+    <div>supported by
+    <Comment />
+    <Comment />
+    <Comment />
+    </div>
+  }
+}
+// <div>supported by
+//         <div>
+//         <img src="https://s3.amazonaws.com/uifaces/faces/twitter/91bilal/128.jpg" height="28" width="28"></img>
+//         <a href="">Pierre Conrad</a>
+//         <p>this album was good</p>
+//         </div>
+//         <div>
+//         <img src="https://s3.amazonaws.com/uifaces/faces/twitter/craigelimeliah/128.jpg" height="28" width="28"></img>
+//         <a href="">Toby Toe</a>
+//         <p>way to go, the production blew me away</p>
+//         </div>
+//         <div>
+//         <img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height="28" width="28"></img>
+//         <a href="">Alexandra Garcia</a>
+//         <p>Rare to find something so unique sounding these days</p>
+//         </div>
+// </div>
