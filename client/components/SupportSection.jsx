8bcf258dc2fb4d35787a254f7960f177e2f1131e
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-03-30 18:16:01 -0400

    add component did mount get request to retrieve users

--- client/components/SupportSection.jsx
+++ client/components/SupportSection.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import CommentList from './CommentList.jsx';
 import UserTable from './UserTable.jsx';
+import $ from 'jquery';
 
 class SupportSection extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      method: "GET",
+      url: "/:id",
+      data: data,
+      success: this.setState({
+        users: JSON.parse(data)
+      })
+    });
   }
 
   render() {
