79914fadfeec77e66304b161433e23df7540cf98
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-03-30 18:04:54 -0400

    outline component render structure

--- client/components/SupportSection.jsx
+++ client/components/SupportSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CommentList from './CommentList.jsx';
+import UserTable from './UserTable.jsx';
 
 class SupportSection extends React.Component {
   constructor(props) {
@@ -10,22 +11,7 @@ class SupportSection extends React.Component {
     return (
       <div>
         <CommentList />
-        <table height="355px" width="250">
-        <tbody>
-        <tr>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        <td height="28" width="28"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img></td>
-        </tr>
-        </tbody>
-        </table>
+        <UserTable />
       </div>
     )
   }
---
+++ client/components/UserThumbnail.jsx
@@ -0,0 +1,1 @@
+<img src="https://s3.amazonaws.com/uifaces/faces/twitter/marakasina/128.jpg" height ="28" width="28"></img>
