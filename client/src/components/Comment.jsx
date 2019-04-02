a65356ccea9d584cec8142baa17a11dd8264c79c
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-02 17:53:54 -0400

    add style to comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -9,8 +9,8 @@ class Comment extends React.Component {
     return (
       <div>
         <img src={this.props.user.profileImageURL} height="28" width="28"></img>
-        <a href="">{this.props.user.username}</a>
-        <p>{this.props.user.comment}</p>
+        <a className="username" href="">{this.props.user.username}</a>
+        <p className='comment'>{this.props.user.comment}</p>
       </div>
     )
   }
