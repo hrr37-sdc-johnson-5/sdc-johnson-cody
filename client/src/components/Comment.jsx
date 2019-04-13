43d8bcb9dbeb1c4f00359df67805b5207d4db037
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-13 19:50:46 -0400

    add indentation

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -7,17 +7,16 @@ class Comment extends React.Component {
 
   render() {
     return (
-
       <div className="commentBlock">
         <div className="pic">
-        <img src={this.props.user.profileImageURL} height="28" width="28"></img>
+          <img src={this.props.user.profileImageURL} height="28" width="28"></img>
         </div>
         <div className="words comment">
-        <a className="username" href="">{this.props.user.username}</a>
-        {this.props.user.comment}
+          <a className="username" href="">{this.props.user.username}</a>
+          {this.props.user.comment}
         </div>
       </div>
-    )
+    );
   }
 }
 
