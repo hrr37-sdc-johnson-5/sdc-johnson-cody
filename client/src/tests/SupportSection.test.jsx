7c76b3bb7aa38f74ec36f706738f117439034dab
Derrick Brandon <DerrickDBrandon@gmail.com>
2019-04-13 20:07:19 -0400

    fix indentation

--- client/src/tests/SupportSection.test.jsx
+++ client/src/tests/SupportSection.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SupportSection from '../components/SupportSection.jsx';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 test('<SupportSection/>', () => {
   let wrapper = shallow(<SupportSection/>);
--- client/src/tests/UserThumbnail.test.jsx
+++ client/src/tests/UserThumbnail.test.jsx
@@ -4,5 +4,6 @@ import {shallow} from 'enzyme';
 
 test('<UserThumbnail/>', () => {
   let wrapper = shallow(<UserThumbnail user={{id: 1, username:"testUser", comment:"test", profileImageURL:"https://s3.amazonaws.com/uifaces/faces/twitter/edgarchris99/128.jpg", album: 1}}/>);
+
   expect(wrapper.exists()).toBe(true);
 });
