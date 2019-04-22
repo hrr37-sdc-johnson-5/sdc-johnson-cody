93172d9e3a841cdc9a3127955b266f2eb584c623
Cody Smith <cms287@gmail.com>
2019-04-22 15:46:44 -0700

    flattened data structure per group discussion

--- database/mockDataGen.js
+++ database/mockDataGen.js
@@ -3,46 +3,27 @@ const fs = require('fs');
 const csvWriter = require('csv-write-stream');
 const writer = csvWriter();
 
-// const generateRandomComments = () => {
-//   randomNumber = faker.random.number({
-//     min: 1,
-//     max: 5
-//   });
-//   let comments = [];
-//   for (let i = 0; i < randomNumber; i++) {
-//     comments.push({
-//       username: faker.internet.userName(),
-//       comment: faker.lorem.sentence(),
-//       profileImgUrl: faker.internet.avatar()
-//     });
-//   }
-//   return JSON.stringify(comments);
-// }
-
-
-// let randomComments = generateRandomComments();
-// console.log(randomComments);
-// console.log(randomComments.slice(0, randomComments.length - 2).split(";,"));
-
 const createCSVdata = () => {
-  console.time('timing seed');
+  console.time('timing gen');
   writer.pipe(fs.createWriteStream('comments.csv'));
   for (let i = 0; i < 10000000; i++) {
-    const album_id = i;
-    const commentsObj = JSON.stringify({
-      username: faker.internet.userName(),
-      comment: faker.lorem.sentence(),
-      profileImgUrl: faker.internet.avatar()
-    });
+    const randomId = faker.random.number({
+          min: 1,
+          max: 10000000
+        });
+    const username = faker.internet.userName();
+    const comment = faker.lorem.sentence();
+    const profileImg = faker.internet.avatar();
     writer.write({
-      album_id,
-      commentsObj
+      albumId: randomId,
+      username: username,
+      comment: comment,
+      profileImg: profileImg
     })
   }
-
   writer.end();
   console.log('done!');
-  console.timeEnd('timing seed');
+  console.timeEnd('timing gen');
 }
 
 createCSVdata();
